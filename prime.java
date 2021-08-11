245992a36f92b213c00a9ae8ed9def4e2d13922a
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-11 19:50:29 +0530

    pr 2

---
+++ prime.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class prime {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int p=0;
+        int n=sc.nextInt();
+        for (int i=1;i<=n/2;i++){
+            if(n%i==0){
+                p++;
+            }
+        }
+
+        if(p==1){
+            System.out.println("The number is prime : ");
+        }
+        else
+        System.out.println("The number is not prime : ");
+    }
+}
