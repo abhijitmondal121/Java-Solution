f91566a71036548127372090a2edf50f074de195
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-12 20:33:41 +0530

    pr 11

---
+++ starPiramid.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class starPiramid {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the range : ");
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            for (int s=n-i;s>1;s--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
