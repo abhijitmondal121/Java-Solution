59a730a4943f8ddc2c8dadf9aab6847a6a7e7928
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-12 20:52:54 +0530

    pr 11

---
+++ starDimond.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+
+public class starDimond {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the range : ");
+        int n=sc.nextInt();
+        for (int i=0;i<n;i++){
+            for (int s=n-i;s>1;s--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=n-1;i>0;i--){
+            for (int s=0;s<n-i;s++){
+                System.out.print(" ");
+            }
+            for (int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
