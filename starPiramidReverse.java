784191e8878687ef488c59424d6c0fc2637d09a6
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-12 21:24:04 +0530

    pr 14

---
+++ starPiramidReverse.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class starPiramidReverse {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the range : ");
+        int n=sc.nextInt();
+        for (int i=n;i>0;i--){
+            for (int s=0;s<n-i;s++){
+                System.out.print(" ");
+            }
+            for (int j=i;j>0;j--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+
+
+
+        for (int i=0;i<n;i++){
+            for (int s=n-i;s>1;s--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
