2c9cc8baa24c50a570ddec8ff1d75577438e88d8
abhijitmondal121 <mondalavi9836@gmail.com>
2021-08-14 21:33:56 +0530

    pr 26

---
+++ countVowel.java
@@ -0,0 +1,24 @@
+
+public class countVowel {
+    public static void main(String[] args) {
+
+        int vcount=0,cCount=0;
+
+        String str="This is a really simple sentence";
+
+        str=str.toLowerCase();
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i) == 'a' || str.charAt(i) == 'e' || str.charAt(i) == 'i' || str.charAt(i) == 'o' || str.charAt(i) == 'u') {
+
+            vcount++;
+
+            }
+            else if(str.charAt(i) >= 'a' && str.charAt(i)<='z'){
+                cCount++;
+            }
+            }
+        System.out.println("Number of vowels: " + vcount);
+        System.out.println("Number of consonants: " + cCount);
+
+    }
+}
